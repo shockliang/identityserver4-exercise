96d7e1f22bde4d451dea26a398d7675fe37712bf
shockliang <shockwater@gmail.com>
2021-06-29 21:40:50 +0800

    Adding customer model.

---
+++ Bank.Api/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace Bank.Api.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
