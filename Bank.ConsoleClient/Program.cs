a64386b3610f32042ef8c220851043f3345d2734
shockliang <shockwater@gmail.com>
2021-07-05 22:09:46 +0800

    Adding console client.

---
+++ Bank.ConsoleClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bank.ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+             
+        }
+    }
+}
