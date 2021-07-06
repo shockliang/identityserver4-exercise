fd3e17a0e7f4bd4db77fcb9ec49e4738cd9ac594
shockliang <shockwater@gmail.com>
2021-07-07 01:20:11 +0800

    Consume secure api.

--- Bank.ConsoleClient/Program.cs
+++ Bank.ConsoleClient/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Bank.ConsoleClient
 {
@@ -36,6 +40,34 @@ namespace Bank.ConsoleClient
             }
 
             Console.WriteLine(tokenResponse.Json);
+
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(new {Id = 10, FirstName = "Shock", LastName = "Liang"}),
+                Encoding.UTF8, 
+                "application/json");
+
+            var createCustomerResponse = await httpClient.PostAsync(
+                "https://localhost:4001/api/customers", customerInfo);
+
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+                return;
+            }
+
+            var getCustomersResponse = await httpClient.GetAsync("https://localhost:4001/api/customers");
+            if (!getCustomersResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomersResponse.StatusCode);
+                return;
+            }
+
+            var content = await getCustomersResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+
+            Console.Read();
+
         }
     }
 }
