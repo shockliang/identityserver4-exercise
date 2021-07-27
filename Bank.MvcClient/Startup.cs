4f4c05676203e65e2a10318261389cb666f96588
shockliang <shockwater@gmail.com>
2021-07-28 00:24:16 +0800

    Configure MVC client authentication.

--- Bank.MvcClient/Startup.cs
+++ Bank.MvcClient/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -24,6 +25,25 @@ namespace Bank.MvcClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ClientId = "mvc";
+                    options.SaveTokens = true;
+                    
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
