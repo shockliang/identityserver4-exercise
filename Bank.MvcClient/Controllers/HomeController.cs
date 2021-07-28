71db94870a33fb9fa0144dafad73c413c44ef28c
shockliang <shockwater@gmail.com>
2021-07-29 00:30:27 +0800

    Add a secure action and view.

--- Bank.MvcClient/Controllers/HomeController.cs
+++ Bank.MvcClient/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Bank.MvcClient.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Bank.MvcClient.Controllers
 {
@@ -24,6 +25,12 @@ namespace Bank.MvcClient.Controllers
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secure()
         {
             return View();
         }
